95000cb44afa9729301acf85d1d2f69e222fe0a2
Juho Jaakkola <juho@elgg.org>
2016-01-10 12:28:49 +0200

    List of online users is now displayed only to logged in users

--- views/default/live_chat/users.php
+++ views/default/live_chat/users.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!elgg_is_logged_in()) {
+	return;
+}
+
 $friends = elgg_list_entities_from_relationship(array(
 	'relationship' => 'friend',
 	'relationship_guid' => elgg_get_logged_in_user_guid(),
