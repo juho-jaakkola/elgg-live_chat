4ab00a06ab6a02aa0e15d0675e4a791c8b95cad7
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2016-02-01 14:45:41 +0200

    Removes pagination and banned users from the friend list

--- views/default/live_chat/bar.php
+++ views/default/live_chat/bar.php
@@ -4,6 +4,7 @@ if (!elgg_is_logged_in()) {
 	return;
 }
 
+$dbprefix = elgg_get_config('dbprefix');
 $friends = elgg_list_entities_from_relationship(array(
 	'relationship' => 'friend',
 	'relationship_guid' => elgg_get_logged_in_user_guid(),
@@ -11,6 +12,12 @@ $friends = elgg_list_entities_from_relationship(array(
 	'type' => 'user',
 	'item_view' => 'live_chat/user',
 	'full_view' => false,
+	'joins' => array("JOIN {$dbprefix}users_entity ue ON e.guid = ue.guid"),
+	'order_by' => 'ue.name ASC',
+	'no_results' => elgg_echo('friends:none'),
+	'wheres' => array('banned = "no"'),
+	'pagination' => false,
+	'limit' => false,
 ));
 
 $module = elgg_view_module('featured', elgg_echo('friends'), $friends, array(
